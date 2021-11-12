3490a3a9f7c5e7e29ca28253456d7dc192cf6162
azim254 <verdixconie@gmail.com>
2021-11-12 15:04:27 +0300

    fixed colapse bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,25 @@
-document.addEventListener("DOMContentLoaded", function(){
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 30) {
-          document.getElementById('navbar_top').classList.add('fixed-top');
-          // add padding top to show content behind navbar
-          navbar_height = document.querySelector('.navbar').offsetHeight;
-          document.body.style.paddingTop = navbar_height + 'px';
-        } else {
-          document.getElementById('navbar_top').classList.remove('fixed-top');
-           // remove padding top from body
-          document.body.style.paddingTop = '0';
-        } 
-    });
-  }); 
+
+
+  /* Navigation*/
+// Collapse the navbar by adding the top-nav-collapse class
+window.onscroll = function () {
+	scrollFunction();
+	scrollFunctionBTT(); // back to top button
+};
+
+window.onload = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.documentElement.scrollTop > 30) {
+		document.getElementById("navbar_top").classList.add("fixed-top");
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector('.navbar').offsetHeight;
+      document.body.style.paddingTop = navbar_height + 'px';
+	} else if ( document.documentElement.scrollTop < 30 ) {
+		document.getElementById("navbar_top").classList.remove("fixed-top");
+        // remove padding top from body
+        document.body.style.paddingTop = '0';
+	}
+}
